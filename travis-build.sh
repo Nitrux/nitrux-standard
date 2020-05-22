d60e4b18b59b053a3e4931e040d666ee5a15226c
luis-lavaire <llavaire01@gmail.com>
2020-05-21 20:51:03 -0600

    removed useless code.

--- travis-build.sh
+++ travis-build.sh
@@ -5,21 +5,18 @@ apt-get --yes install wget equivs curl git
 
 cd package/
 
-DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
+deps=$(sed -r '/^(#.*|\s)*$/d; $! s/$/,/' package/dependencies | tr -d \\n)
+git_commit=$(git rev-parse --short HEAD)
 
-GIT_COMMIT=$(git rev-parse --short HEAD)
-
-echo "
-Section: misc
-Priority: optional
-Homepage: https://nxos.org
-
-Package: nitrux-standard
-Version: 0.1.14-${GIT_COMMIT}
-Maintainer: Uri Herrera <uri_herrera@nxos.org>
-Depends: $DEPENDS
-Architecture: amd64
-Description: The Nitrux standard system.
-" > configuration
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: nitrux-standard" \
+	"Version: 0.1.14-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Architecture: amd64" \
+	"Description: The Nitrux standard system."
 
 equivs-build configuration
