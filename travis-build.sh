4da622f258cc1c7b85aa1c267c803af11a4c351f
Uri Herrera <hello@uriherrera.com>
2020-08-15 21:14:15 -0500

    fix pkg name

--- travis-build.sh
+++ travis-build.sh
@@ -10,7 +10,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: nitrux-standard" \
+	"Package: nitrux-standard-legacy" \
 	"Version: 0.1.13-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
