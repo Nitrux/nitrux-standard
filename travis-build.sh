54a5ab31b595eb314ad79610c0fe0f5cdd641fda
Uri Herrera <hello@uriherrera.com>
2020-08-18 20:30:04 -0500

    rm dep

--- travis-build.sh
+++ travis-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-standard-legacy" \
-	"Version: 0.1.14-$git_commit" \
+	"Version: 0.1.15-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: amd64" \
